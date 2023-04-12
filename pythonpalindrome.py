474362a3cc1dd0cd6e0aa9adc8a447b41e4de8c3
Sumanth22001A0545 <129852555+Sumanth22001A0545@users.noreply.github.com>
2023-04-12 15:46:55 +0530

    Checking whether a string is palindrome or not

--- pythonpalindrome.py
+++ pythonpalindrome.py
@@ -1,8 +1,15 @@
-a=int(input("Enter thee number: "))
+a=input("Enter the string: ")
+n=len(a)-1
 i=0
-while(a>0):
-    digit=a%10
+m=0
+while (i<n):
+    if (a[i] is a[n]):
+        m=m+1
     i=i+1
-    a=a/10
+    n=n-1
+if (m==n):
+    print(a," is  a palindrome string")
+else :
+    print(a,"is not a palindrome string")
 
-print(i)
+    
